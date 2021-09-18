02455060cffcaa5f28d577b46858678df9f2b129
Aizirek Nurlanbekova <aizirek.nurlanbekova.pls@gmail.com>
2021-09-18 14:55:02 +0600

    lesson-57

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
+import store from "./redux/store";
 import App from './App';
+
+const increment = ()=> {
+  store.dispatch({
+    type: "counter/incremented"
+  })
+};
+store.subscribe(()=> {
+  console.log("changed:", store.getState());
+})
+const decrement = ()=> {
+  store.dispatch({
+    type: "counter/decremented"
+  })
+};
+
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <button onClick = {decrement}>SUBTR - 1</button>
+    <button onClick = {increment}>ADD + 1</button>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
